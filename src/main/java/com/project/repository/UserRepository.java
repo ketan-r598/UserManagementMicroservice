f25883ba872dd581855eab6921515070027070d9
Ketan R <ketan_r598@outlook.com>
2023-06-22 00:06:51 +0530

    Added custom Queries

--- src/main/java/com/project/repository/UserRepository.java
+++ src/main/java/com/project/repository/UserRepository.java
@@ -11,4 +11,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	public Optional<User> findByEmailAndPassword(String email, String password);
 	public void deleteByEmailAndPassword(String email, String password);
 	public Optional<User> findByEmail(String email);
+	public void deleteByEmail(String email);
 }
