976e9f57babde61ba4a30947ace19e5e2d3ad620
Ketan R <ketan_r598@outlook.com>
2023-06-22 00:04:58 +0530

    Update Token Generator to generate custom tokens

--- src/main/java/com/project/service/JWTTokenGeneratorService.java
+++ src/main/java/com/project/service/JWTTokenGeneratorService.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.project.model.UserCredentials;
+import com.project.session.Session;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -18,13 +18,25 @@ public class JWTTokenGeneratorService implements TokenGeneratorService  {
 	private String secretKey;
 
 	@Override
-	public Map<String, String> generateToken(UserCredentials user) {
-		String token = Jwts.builder().setSubject(user.getEmail())
-		.setIssuer("ContactAppIssuer")
-		.setIssuedAt(new Date())
-		.signWith(SignatureAlgorithm.HS256, secretKey)
-		.compact();
+	public Map<String, String> generateToken(Session user) {
+		String token = Jwts
+				.builder()
+				.setSubject(user.getEmail())
+				.claim("userId", user.getUserId())
+				.claim("password", user.getPassword())
+				.claim("role",user.getRole())
+				.setIssuer("UserMicroservice")
+				.setIssuedAt(new Date())
+				.signWith(SignatureAlgorithm.HS256, secretKey)
+				.compact();
+		
+		System.out.println();
+		System.out.println("userID = "+ user.getUserId());
+		System.out.println("userEmail = "+user.getEmail());
+		System.out.println("userPassword = "+user.getPassword());
+		System.out.println("userRole = "+user.getRole());
+		System.out.println();
+		
 		return Map.of("token",token);
 	}
-
 }
